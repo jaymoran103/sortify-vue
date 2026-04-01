f3e6eff8fe8c10a5df769d87697eb88728345ab9
Jay Moran <jaymorandev@gmail.com>
2026-04-01 15:42:15 -0500

    added global styles and design tokens; include reset, utility, and variable CSS files

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import './styles/variables.css'
+import './styles/reset.css'
+import './styles/utilities.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
